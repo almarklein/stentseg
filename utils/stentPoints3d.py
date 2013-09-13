facf1601b177198576c083912959057d6b2f3191
Almar Klein <almar.klein@gmail.com>
2013-09-13 13:50:59 +0200

    Rewrote point_detect_3d in Python, which is only twices as slow as the Cython version!

--- utils/stentPoints3d.py
+++ utils/stentPoints3d.py
@@ -16,7 +16,7 @@ from visvis import Point, Pointset, Aarray
 from visvis.utils.pypoints import is_Aarray
 
 
-from pirt import gaussfun
+from stentseg import gaussfun
 
 
 def getStentSurePositions(data, th1):
@@ -58,135 +58,47 @@ def getStentSurePositions(data, th1):
     return pp
 
 
-def thin(data):
-    """ thin(data) 
-    Thin the given binary data, converting all blobs of voxels to single
-    voxels if they are max 3x3(x3). Larger blobs are discarted.
-    
-    This can be used to get rid of local maxima close together...
-    """
-    # create strels
-    strel1 = np.array([0,1,1])
-    strel2 = np.array([1,1,0])
-    
-    # preform reduction of size
-    for i in range(1):
-        for i in range(data.ndim):
-            # make shapes right
-            shape = [1 for tmp in data.shape]; shape[i]=3
-            strel1.shape = tuple(shape)
-            strel2.shape = tuple(shape)
-            # apply "erosion"
-            data2 = sp.ndimage.binary_hit_or_miss(data, strel1)    
-            data = data-data2
-            data2 = sp.ndimage.binary_hit_or_miss(data, strel2)
-            data = data-data2
-    
-    # keep only singleton pixels
-    strel3 = np.zeros([3 for tmp in data.shape])
-    index = tuple( [1 for tmp in data.shape] ) 
-    strel3[index] = 1
-    data = sp.ndimage.binary_hit_or_miss(data,strel3)
-    return data
 
 
-def getStentSurePositions_wrong(data, th1, th2=0):
-    """ getStentSurePositions(data, th)
-    Based on three measures, establish positions where 
-    we can be sure to have a stent's wire:
-    - intensity above given threshold
-    - local maximum
-    - at least one neighbour with intensity above threshold
+def detect_stent_points_3d(data, th):
     
-    Another nice feature is that these points will also often be found
-    on (in the centre of) crossings.
-    """
+    mask = np.zeros_like(data, np.uint8)
     
-    # ===== THE DISCREPANCY WITH THE CYTHON IMPLEMENTATION ====
-    # The Cython implementation finds slightly less points. Due to the extra
-    # points found by the Python implementation, there are considerably more
-    # edges in the end result, which are mostly to be on the spinal cord.
-    # (Use the piece of code in the demo of  stentDirect.py that removes
-    # most of these to see the difference.)
-    # With the Cython implementation, the method scores a few per cent better
-    # for the Aneurx.
-    # 
-    # Number of seedpoints in different situations
-    # Python on Zenith: 1789
-    # Cython on Zenith: 1732
-    # Python on Aneurx: 3007  /w cleanup: 1187
-    # Cython on Aneurx: 2873  /w cleanup: 1156
-    # Python without thinning: 3043
-    # Cython without thinning: 3043
-    # --> It seems the thinning operation of Python does not work very well
+    # Criterium 1: voxel must be above th
+    # Note that we omit the edges
+    mask[1:-1,1:-1,1:-1] = (data[1:-1,1:-1,1:-1] > th) * 3
     
-    # todo: thresholds
-    # - a threshold of 1000 works for pat01
-    # - but for pat12 around 700 is better
-    # - 500 works allright also
-    # - but 500 on pat01 will find many point in contrast fluid
-    # - pat13 needs th1<=500
     
-    sampling = (1,1,1)
-    if is_Aarray(data):
-        sampling = data.sampling
+    for z, y, x in zip(*np.where(mask==3)):
+        
+        # Only proceed if this voxel is "free"
+        if mask[z,y,x] == 3:
+            
+            # Set to 0 initially
+            mask[z,y,x] = 0  
+            
+            # Get value
+            val = data[z,y,x]
+            
+            # Get maximum of neighbours
+            patch = data[z-1:z+2, y-1:y+2, x-1:x+2].copy()
+            patch[1,1,1] = 0
+            themax = patch.max()
+            
+            # Criterium 2: must be local max
+            if themax > val:
+                continue
+            # Also ensure at least one neighbour to be *smaller*
+            if (val > patch).sum() == 0:
+                continue
+            
+            # Criterium 3: one neighbour must be above th
+            if themax <= th:
+                continue
+            
+            # Set, and suppress stent points at direct neightbours
+            mask[z-1:z+2, y-1:y+2, x-1:x+2] = 1
+            mask[z,y,x] = 2
     
-    # calculate Laplacian
-    if th2:
-        sigma = 0.8 # in mm
-        # take anisotropy into account
-        sigmas = [sigma/sam for sam in sampling]
-        Lxx = gaussfun.gfilter(data, sigmas, [0,0,2])
-        Lyy = gaussfun.gfilter(data, sigmas, [0,2,0])
-        Lzz = gaussfun.gfilter(data, sigmas, [2,0,0])
-        Lap = -(Lxx + Lyy + Lzz)
-        #Lap = Aarray( Lap, sampling)
-    
-    # init pointset
-    pp = Pointset(data.ndim) 
-    
-    # create localmax array
-    localmax = data.copy()
-    localmax[data<th1] = 0    
-    kernel = np.ones(3**data.ndim); kernel[(kernel.shape[0]-1)/2] = 0
-    kernel.shape = {2:(3,3), 3:(3,3,3)}[pp.ndim]
-    localmax = sp.ndimage.maximum_filter(localmax,footprint=kernel)
-    
-    # get voxels that are ok
-    if th2:
-        mask = (    (data>0) & # min th
-                    ((data-localmax)>=0) & # is-local-max
-                    (localmax>th1) & # data larger than th1
-                    (Lap>th2)  ) # Laplacian larger than th2        
-    else:
-        mask = (    (data>0) & # min th
-                    ((data-localmax)>=0) & # is-local-max
-                    (localmax>th1)  ) # larger than th
-    
-    # reduce groups to single pixel
-    #mask = thin(mask)
-    
-    # convert mask to points
-    indices = np.where(mask)
-    if pp.ndim==2:
-        Iy, Ix = indices
-        for i in range(len(Ix)):
-            pp.append(Ix[i], Iy[i])
-    elif pp.ndim==3:
-        Iz, Iy, Ix = indices
-        for i in range(len(Ix)):
-            pp.append(Ix[i], Iy[i], Iz[i])
-    else:
-        raise ValueError('Can only handle 2D and 3D data.')
-    
-    # correct for anisotropy
-    if is_Aarray(data):
-        scale = Point( tuple(reversed(data.sampling)) ) 
-        if hasattr(data, 'get_start'):            
-            offset = data.get_start()
-        else:
-            offset = data.get_start()
-        pp = pp * scale + offset
-    
-    # done
-    return pp
+    return mask
+
