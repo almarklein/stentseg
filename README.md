164d6571822c355655322855b4194b80dfb5cc0d
Almar Klein <almar.klein@gmail.com>
2014-01-10 16:22:54 +0100

    license and more

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ during my PhD. The only algorithmm that is exposed is the 3-step
 stent-direct algorithm.
 
 This library is written for Python 3, and probably also works for Python 2.
+It is written in pure Python, so installation is easy.
+
+If you publish research in which this software is used, we would 
+appreciate it if you refer to our paper:
+    
+    Klein, Almar and van der Vliet, J.A. and Oostveen, L.J. and Hoogeveen, 
+    Y. and Schultze Kool, L.J. and Renema, W.K.J. and Slump, C.H. (2012) 
+    Automatic segmentation of the wire frame of stent grafts from CT data. 
+    Medical Image Analysis, 16 (1). pp. 127-139. ISSN 1361-8415
+
 
 ## Installation
 
--- stentseg/__init__.py
+++ stentseg/__init__.py
@@ -8,12 +8,16 @@ stent grafts. These represent the different attempt that I did
 during my PhD. The only algorithmm that is exposed is the 3-step
 stent-direct algorithm.
 
-In the current situation the algorithms are assumed to return a model
-of the graph based on a geometric graph, but additional models may be
-added later.
-
 This library is written for Python 3, and probably also works for Python 2.
 It is written in pure Python, so installation is easy.
+
+If you publish research in which this software is used, we would 
+appreciate it if you refer to our paper:
+    
+    Klein, Almar and van der Vliet, J.A. and Oostveen, L.J. and Hoogeveen, 
+    Y. and Schultze Kool, L.J. and Renema, W.K.J. and Slump, C.H. (2012) 
+    Automatic segmentation of the wire frame of stent grafts from CT data. 
+    Medical Image Analysis, 16 (1). pp. 127-139. ISSN 1361-8415
 
 """
 
