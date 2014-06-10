cd836818ff590935d424506c4da8dca510580893
maaikekoenrades <maaikekoenrades@localhost>
2014-06-10 02:45:08 +0200

    create_mesh_with_values in progress; Progress to show thinner/transparant proximal ring in moving mesh; remove_stent_from_volume modified, works with path points, works with new mesh model class

--- lspeas/_show_model.py
+++ lspeas/_show_model.py
@@ -8,7 +8,9 @@ import visvis as vv
 from stentseg.utils.datahandling import select_dir, loadvol, loadmodel
 from pirt.utils.deformvis import DeformableTexture3D, DeformableMesh
 from stentseg.stentdirect.stentgraph import create_mesh
-
+from stentseg.motion.vis import create_mesh_with_values,remove_stent_from_volume 
+#import skimage.morphology
+from skimage.morphology import reconstruction
 
 # Select the ssdf basedir
 basedir = select_dir(os.getenv('LSPEAS_BASEDIR', ''),
@@ -23,6 +25,8 @@ cropname = 'ring'
 s = loadmodel(basedir, ptcode, ctcode, cropname)
 model = s.model
 modelmesh = create_mesh(model, 0.6)  # Param is thickness
+#modelmesh = create_mesh_with_values(model, 0.6, fullPaths=True)
+#todo: create mesh based on path and deforms -> which points deform most?
 
 # Load deformations
 s = loadvol(basedir, ptcode, ctcode, cropname, 'deforms')
@@ -32,10 +36,27 @@ deforms = [[field[::2,::2,::2] for field in fields] for fields in deforms]
 # todo: the deforms are stored in backward mapping (I think)
 # so we need to transform them to forward here.
 
+
+# Load static CT image to add as reference
+s = loadvol(basedir, ptcode, ctcode, cropname, 'avgreg')
+vol = s.vol
+
+# Remove stent from vol for visualization
+vol = remove_stent_from_volume(vol, model, stripSize=4)
+
+# todo: also create a way to show static ring thinner/transparent as reference 
+# skimage.morphology.reconstruction(seed, mask, method='dilation', selem=None, offset=None)
+# seed = vol # seed image is eroded
+# mask = np.zeros_like(vol, np.uint8)
+# mask[np.where(vol < 2500)] = 2500
+# vol2 = reconstruction(seed, mask, method='erosion')
+
 # Start vis
 f = vv.figure(1); vv.clf()
 a = vv.gca()
 a.daspect = 1, -1, -1
+t = vv.volshow(vol, clim=(0, 2500), renderStyle='mip')
+vv.ColormapEditor(vv.gcf())
 
 # Create deformable mesh
 dm = DeformableMesh(a, modelmesh)
@@ -46,9 +67,10 @@ a.SetLimits()
 dm.MotionPlay(10, 0.2)  # Each 10 ms do a step of 20%
 dm.motionSplineType = 'B-spline'
 dm.motionAmplitude = 3.0  # For a mesh we can (more) safely increase amplitude
+dm.faceColor = 'g'
 
 
-# todo: add static CT image for reference. Nice excersise for Maaike :)
+#vv.record(a)
 
 # In stentseg.motion.vis are a few functions, but they need to be adjusted
 # to work with the new stent model.
--- stentseg/motion/vis.py
+++ stentseg/motion/vis.py
@@ -2,36 +2,82 @@
 """
 
 
-def create_mesh_with_values(g, radius=1.0, simplified=True): 
+# def create_mesh_with_values(g, radius=1.0, simplified=True): 
+#     """ Create a Mesh object from the graph. The values of the mesh
+#     encode triplets (node1, node2, weight) where node1 and node2 are
+#     indices to nodes in the graph and weight is the relative proximity
+#     to these nodes.
+#     
+#     The values array can then be used to set the value to any kind
+#     of information derived from the nodes.
+#     
+#     """
+#     from visvis.processing import lineToMesh, combineMeshes
+#     # todo: this still uses the old mesh model class
+#     
+#     # Init list of meshes
+#     meshes = []
+#     
+#     for e in g.GetEdges():
+#         # Obtain path of edge and make mesh
+#         if simplified:
+#             # Straight path
+#             path, values = vv.Pointset(3), vv.Pointset(3)
+#             path.append(e.end1._data); values.append(e._i1, e._i2, 0)
+#             path.append(e.end2._data); values.append(e._i1, e._i2, 1)
+#             #values = [e.end1._angleChange, e.end2._angleChange]
+#         else:
+#             path = vv.Pointset(e.props[2].data)
+#             #values = values.reshape(-1, 1)
+#             values = vv.Pointset(3)
+#             for i in np.linspace(0.0, 1.0, len(path)):
+#                 values.append(e._i1, e._i2, i)
+#         meshes.append( lineToMesh(path, radius, 8, values) )
+#     
+#     # Combine meshes and return
+#     if meshes:
+#         return combineMeshes(meshes)
+#     else:
+#         return None
+
+# Modify
+def create_mesh_with_values(graph, radius=1.0, fullPaths=True): 
     """ Create a Mesh object from the graph. The values of the mesh
     encode triplets (node1, node2, weight) where node1 and node2 are
-    indices to nodes in the graph and weight is the relative proximity
-    to these nodes.
+    indices to nodes in the graph and weight is the deformation at these nodes.
     
-    The values array can then be used to set the value to any kind
+    The values array can be used to set the value to any kind
     of information derived from the nodes.
     
     """
     from visvis.processing import lineToMesh, combineMeshes
-    # todo: this still uses the old mesh model class
+    from visvis import Pointset  # lineToMesh does not like the new PointSet class
+    import numpy as np
+    # todo: this still uses the old mesh model class: in progress..
     
     # Init list of meshes
     meshes = []
     
-    for e in g.GetEdges():
+    for n1,n2 in graph.edges():
         # Obtain path of edge and make mesh
-        if simplified:
-            # Straight path
-            path, values = vv.Pointset(3), vv.Pointset(3)
-            path.append(e.end1._data); values.append(e._i1, e._i2, 0)
-            path.append(e.end2._data); values.append(e._i1, e._i2, 1)
-            #values = [e.end1._angleChange, e.end2._angleChange]
-        else:
-            path = vv.Pointset(e.props[2].data)
+        if fullPaths:
+            path = graph.edge[n1][n2]['path']
+            path = Pointset(path)  # Make a visvis pointset
+            values = Pointset(3)
             #values = values.reshape(-1, 1)
-            values = vv.Pointset(3)
+            movn1 = np.asarray(sum(abs(graph.node[n1]['deforms']))) # x, y, z (?)
+            movn2 = np.asarray(sum(abs(graph.node[n2]['deforms'])))
             for i in np.linspace(0.0, 1.0, len(path)):
-                values.append(e._i1, e._i2, i)
+                values.append(movn1); values.append(movn2)
+        else:
+            # Straight path
+            path, values = Pointset(3), Pointset(3)
+            path.append(n1); path.append(n2)
+            movn1 = np.asarray(sum(abs(graph.node[n1]['deforms'])))
+            movn2 = np.asarray(sum(abs(graph.node[n2]['deforms'])))
+            values.append(movn1); values.append(movn2)
+            #values = [n1._angleChange, n2._angleChange]
+            
         meshes.append( lineToMesh(path, radius, 8, values) )
     
     # Combine meshes and return
@@ -68,18 +114,20 @@ def convert_mesh_values_to_angle_change(m, g, i=None):
     return F1 * angleChanges[I1] + F2 * angleChanges[I2]
 
 
-def remove_stent_from_volume(vol, g, stripSize=5):
-    """ Make the high intensity voxels that belong to the stent of a
+def remove_stent_from_volume(vol, graph, stripSize=5):
+    """ Give the high intensity voxels that belong to the stent a
     lower value, so that the stent appears to be "removed". This is for
-    visualization purposes only.
+    visualization purposes only. Makes use of known paths in graph model.
     """
-    # todo: this still uses the old mesh model class
+    
+    from visvis import Pointset
+
     vol2 = vol.copy()
-    for e in g.GetEdges():
-        for node in e.GetPath(e.end1):
-    #for node in g:
-            z,y,x = vol2.point_to_index(node)
-            stripSize2 = 2 * stripSize
+    for n1,n2 in graph.edges():
+        path = graph.edge[n1][n2]['path']
+        path = Pointset(path)  # Make a visvis pointset
+        stripSize2 = 2 * stripSize
+        for point in path:
+            z,y,x = vol2.point_to_index(point)
             vol2[z-stripSize:z+stripSize+1, y-stripSize:y+stripSize+1, x-stripSize:x+stripSize+1] = 0
     return vol2
-
