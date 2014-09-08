3305f1d4e8be736e660e14d45dcea7415e318dca
maaikekoenrades <maaikekoenrades@localhost>
2014-09-08 13:20:02 +0200

    update to current use add comment that seg overwrites add s2.stentType to model, ref to how was segmented

--- lspeas/_do_segmentation.py
+++ lspeas/_do_segmentation.py
@@ -1,5 +1,6 @@
 """ Script to do the segmentation and store the result.
 
+Do not run file but execute cells (overwrites!)
 """
 
 import os
@@ -18,10 +19,10 @@ basedir = select_dir(os.getenv('LSPEAS_BASEDIR', ''),
                      r'C:\Users\Maaike\Documents\UT MA3\LSPEAS_ssdf',)
 
 # Select dataset to register
-ptcode = 'LSPEAS_002'
-ctcode = '1month'
+ptcode = 'LSPEAS_003'
+ctcode = 'discharge'
 cropname = 'ring'
-what = 'avg3090'
+what = 'avgreg'
 
 
 ## Perform segmentation
@@ -35,16 +36,16 @@ stentType = 'anaconda'  # 'anacondaRing' runs stentgraph_anacondaRing.prune_redu
 popNodes = True  # True when NOT using GUI
 
 p = getDefaultParams(stentType)
-p.seed_threshold = 1000                 # step 1
-p.mcp_speedFactor = 190                 # step 2, speed image (delta), costToCtValue
+p.seed_threshold = 1500                 # step 1
+p.mcp_speedFactor = 170                 # step 2, speed image (delta), costToCtValue
 p.mcp_maxCoverageFronts = 0.003         # step 2, base.py; replaces mcp_evolutionThreshold
 p.graph_weakThreshold = 1000             # step 3, stentgraph.prune_very_weak
 p.graph_expectedNumberOfEdges = 3       # step 3, stentgraph.prune_weak
 p.graph_trimLength =  0                 # step 3, stentgraph.prune_tails
 p.graph_minimumClusterSize = 10         # step 3, stentgraph.prune_clusters
-p.graph_strongThreshold = 3000          # step 3, stentgraph.prune_weak and stentgraph.prune_redundant
-#p.graph_min_strutlength = 5            # step 3, stentgraph.prune_weak and stentgraph.prune_redundant
-#p.graph_max_strutlength = 10            # step 3, stentgraph.prune_weak and stentgraph.prune_redundant
+p.graph_strongThreshold = 4600          # step 3, stentgraph.prune_weak and stentgraph.prune_redundant
+# p.graph_min_strutlength = 5            # step 3, stentgraph.prune_weak and stentgraph.prune_redundant
+# p.graph_max_strutlength = 10            # step 3, stentgraph.prune_weak and stentgraph.prune_redundant
 # todo: write function to estimate maxCoverageFronts
 
 # Instantiate stentdirect segmenter object
@@ -65,12 +66,19 @@ model = sd._nodes3
 
 fig = vv.figure(3); vv.clf()
 fig.position = 0, 22, 1366, 706
+viewringcrop = {'azimuth': 105.3965517241379,
+ 'daspect': (1.0, -1.0, -1.0),
+ 'elevation': 31.457725947521887,
+ 'fov': 0.0,
+ 'loc': (98.97817014708237, 103.62329297851174, 111.17764009804003),
+ 'roll': 0.0,
+ 'zoom': 0.025718541865111917}
 
 # Show volume and model as graph
 a1 = vv.subplot(131)
 t = vv.volshow(vol)
 t.clim = 0, 2500
-#sd._nodes1.Draw(mc='g', mw = 6)       # draw seeded nodes
+# sd._nodes1.Draw(mc='g', mw = 6)       # draw seeded nodes
 sd._nodes2.Draw(mc='g', lc = 'g')    # draw seeded and MCP connected nodes
 
 # Show volume and cleaned up graph
@@ -88,19 +96,15 @@ m = vv.mesh(bm)
 m.faceColor = 'g'
 vv.xlabel('x (mm)');vv.ylabel('y (mm)');vv.zlabel('z (mm)')
 
-# # Use same camera
+# Use same camera
 a1.camera = a2.camera = a3.camera
-
-viewringcrop = {'azimuth': 103.99999999999996,
- 'daspect': (1.0, -1.0, -1.0),
- 'elevation': 38.39650145772596,
- 'fov': 0.0,
- 'loc': (133.65612191490567, 178.8566574696272, 67.83158841706212),
- 'roll': 0.0,
- 'zoom': 0.025718541865111865}
 
 a1.SetView(viewringcrop)
 
+switch = False
+a1.axis.visible = switch
+a2.axis.visible = switch
+a3.axis.visible = switch
 
 ## Store segmentation to disk
 
@@ -117,12 +121,13 @@ for key in dir(s):
             s2['meta'+suffix] = s['meta'+suffix]
 s2.what = what
 s2.params = p
+s2.stentType = stentType
 # Store model
 s2.model = model.pack()
 #s2.mesh = ssdf.new()
 
 # Save
-filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, cropname, 'model')
+filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, cropname, 'model'+what)
 ssdf.save(os.path.join(basedir, ptcode, filename), s2)
 
 
@@ -137,7 +142,7 @@ deforms = [s['deform%i'%(i*10)] for i in range(10)]
 deforms = [pirt.DeformationFieldBackward(*fields) for fields in deforms]
 
 # Load model
-s = loadmodel(basedir, ptcode, ctcode, cropname)
+s = loadmodel(basedir, ptcode, ctcode, cropname, 'model'+what)
 model = s.model
 
 # Combine ...
@@ -145,6 +150,6 @@ incorporate_motion_nodes(model, deforms, s.origin)
 incorporate_motion_edges(model, deforms, s.origin)
 
 # Save back
-filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, cropname, 'model')
+filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, cropname, 'model'+what)
 s.model = model.pack()
 ssdf.save(os.path.join(basedir, ptcode, filename), s)
