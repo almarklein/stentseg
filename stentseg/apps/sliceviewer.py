b17af6b235906b13bae2af47566ed2a446ebd4d3
maaikekoenrades <maaikekoenrades@localhost>
2015-10-06 17:06:27 +0200

    Attempt to modify sliceviewer. Fix: incorporate option to draw line profile to get intensities and option to display intensity under mouse, as possible in matlab..

--- stentseg/apps/sliceviewer.py
+++ stentseg/apps/sliceviewer.py
@@ -29,3 +29,30 @@ class VolViewer:
         self.t.SetData(self.vol[self.z])
     
     
+# #todo: incorporate option to draw line profile to get intensities and option to display intensity under mouse
+# from skimage.measure import profile_line
+# img = vol[0,:,:]
+# vv.imshow(img)
+# plt.imshow(img)
+# 
+# im = vv.imread('lena.png')
+# new_viewer = skimage.viewer.ImageViewer(img) 
+# from skimage.viewer.plugins import lineprofile
+# new_viewer += lineprofile.LineProfile() 
+# new_viewer.show()     
+# 
+# 
+# import numpy as np
+# import matplotlib.pyplot as plt
+# 
+# class Formatter(object):
+#     def __init__(self, im):
+#         self.im = im
+#     def __call__(self, x, y):
+#         z = self.im.get_array()[int(y), int(x)]
+#         return 'x={:.01f}, y={:.01f}, z={:.01f}'.format(x, y, z)
+# 
+# fig, ax = plt.subplots()
+# im = ax.imshow(img)
+# ax.format_coord = Formatter(im)
+# plt.show()
