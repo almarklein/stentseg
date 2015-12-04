b5451680e7ec1a96b3e1f055f1f6410ae54b2eec
maaikekoenrades <maaikekoenrades@localhost>
2015-12-04 12:49:03 +0100

    Add prevention of seed placement in markers anacondaRing, step 1

--- stentseg/stentdirect/stent_anaconda.py
+++ stentseg/stentdirect/stent_anaconda.py
@@ -9,6 +9,7 @@ from __future__ import print_function, division, absolute_import
 
 import numpy as np
 import networkx as nx
+import time
 
 import visvis as vv
 from visvis import ssdf
@@ -26,11 +27,48 @@ SORTBY = 'cost'
 
 class AnacondaDirect(StentDirect):
     """ An implementation of the StentDirect algorithm targeted at 
-    the Anaconda stent graft. In particular, the pruning algorithm is
+    the Anaconda dual ring. In particular, the pruning algorithm is
     modified.
     """
     
 #     stentType = 'anaconda'
+
+    def Step1(self):
+        """ Step1()
+        Detect seed points.
+        """
+        
+        # Check if we can go
+        if self._vol is None or self._params is None:
+            raise ValueError('Data or params not yet given.')
+        
+        t0 = time.time()
+        
+        # Detect points
+        th = self._params.seed_threshold
+        pp = get_stent_likely_positions(self._vol, th) # calls function below
+        
+        # Create nodes object from found points
+        nodes = stentgraph.StentGraph()
+        for p in pp:
+            p_as_tuple = tuple(p.flat) # todo: perhaps seed detector should just yield list of tuples.
+            nodes.add_node(p_as_tuple)
+        
+        t1 = time.time()
+        if self._verbose:
+            print()
+            print('Found %i seed points, which took %1.2f s.' % (len(nodes), t1-t0))
+        
+        # Store the nodes
+        self._nodes1 = nodes
+        
+        # Draw?
+        if self._draw:
+            self.Draw(1)
+        
+        return nodes
+
+
     
     def _Step3_iter(self, nodes, cleanNodes=True):
         params = self._params
@@ -56,6 +94,103 @@ class AnacondaDirect(StentDirect):
         stentgraph.prune_tails(nodes, params.graph_trimLength)
         stentgraph.prune_clusters(nodes, params.graph_minimumClusterSize)
 
+
+# Step 1: Rationale: markers are placed next to wire, prevent seed placement here
+def get_stent_likely_positions(data, th):
+    """ Get a pointset of positions that are likely to be on the stent.
+    If the given data has a "sampling" attribute, the positions are
+    scaled accordingly. 
+    
+    Detection goes according to three criteria:
+      * intensity above given threshold
+      * local maximum
+      * at least one neighbour with intensity above threshold
+    """
+    
+    # Get mask
+    mask = get_mask_with_stent_likely_positions(data, th)
+    
+    # Convert mask to points
+    indices = np.where(mask==2)  # Tuple of 1D arrays
+    pp = PointSet( np.column_stack(reversed(indices)), dtype=np.float32)
+    
+    # Correct for anisotropy and offset
+    if hasattr(data, 'sampling'):
+        pp *= PointSet( list(reversed(data.sampling)) ) 
+    if hasattr(data, 'origin'):
+        pp += PointSet( list(reversed(data.origin)) ) 
+    
+    return pp
+
+
+def get_mask_with_stent_likely_positions(data, th):
+    """ Get a mask image where the positions that are likely to be
+    on the stent, subject to three criteria:
+      * intensity above given threshold
+      * local maximum
+      * at least one neighbour with intensity above threshold
+    Returns a mask, which can easily be turned into a set of points by 
+    detecting the voxels with the value 2.
+    
+    This is a pure-Python implementation.
+    """
+    
+    # NOTE: this pure-Python implementation is little over twice as slow
+    # as the Cython implementation, which is a neglectable cost since
+    # the other steps in stent segmentation take much longer. By using
+    # pure-Python, installation and modification are much easier!
+    # It has been tested that this algorithm produces the same results
+    # as the Cython version.
+    
+    print('get mask for seedpoints anacondaRing is used')
+    # Init mask
+    mask = np.zeros_like(data, np.uint8)
+    
+    # Criterium 1: voxel must be above th
+    # Note that we omit the edges
+    mask[3:-3,3:-3,3:-3] = (data[3:-3,3:-3,3:-3] > th) * 3
+    
+    
+    for z, y, x in zip(*np.where(mask==3)):
+        
+        # Only proceed if this voxel is "free"
+        if mask[z,y,x] == 3:
+            
+            # Set to 0 initially
+            mask[z,y,x] = 0  
+            
+            # Get value
+            val = data[z,y,x]
+            
+            # Get maximum of neighbours
+            patch = data[z-1:z+2, y-1:y+2, x-1:x+2].copy()
+            patch[1,1,1] = 0
+            themax = patch.max()
+            
+            # Criterium 2: must be local max
+            if themax > val:
+                continue
+            # Also ensure at least one neighbour to be *smaller*
+            if (val > patch).sum() == 0:
+                continue
+            
+            # Criterium 3: one neighbour must be above th
+            if themax <= th:
+                continue
+            
+            # Criterium 4: seed must not be placed in tantalum marker
+            if val > data.max()*0.5: # seed in tantalum marker
+                print(val)
+                continue
+            
+            # Set, and suppress stent points at direct neighbours
+            mask[z-1:z+2, y-1:y+2, x-1:x+2] = 1
+            mask[z,y,x] = 2
+    
+    return mask
+
+
+# Step 3
 def prune_weak(graph, enc, ctvalue, min_strutlength, max_strutlength):
     """ Remove weak edges, based on cost and number of expected edges (enc).
     
@@ -198,7 +333,6 @@ def _prune_redundant_edge(graph, n1, n2, min_ctvalue, min_strutlength, max_strut
         print('Warning: detected node that was connected to itself.')
         graph.remove_edge(n1, n2)
         return counter
-    
     # Get neighbours for n1 and n2
     nn1 = set(graph.edge[n1].keys())
     nn1.discard(n2)
