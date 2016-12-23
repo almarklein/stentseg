0b64fa5be63e69f8d65743aa9985d50e9326c702
maaikekoenrades <maaikekoenrades@localhost>
2016-12-23 18:07:52 +0100

    modify imports with nellix package

--- nellix/Total.py
+++ nellix/Total.py
@@ -1,32 +1,34 @@
 '''
 Created on 1 jun. 2016
 
-@author: TomLoonen
+@author: TomLoonen / modified MaaikeKoenrades
 '''
 import os
-ptcode = 'chevas_7'
-dicom_basedir = 'E:/CT/no 7/STD00001/10phases'
-basedir = 'E:/CT/SSDF/'
+from stentseg.utils.datahandling import select_dir
+
+ptcode = 'chevas_07'
+dicom_basedir = r'G:\Nellix_chevas\CT\no 7\STD00001\10phases'
+basedir = select_dir('E:/CT/SSDF/', r'G:\Nellix_chevas\CT\SSDF')
 print(dicom_basedir)
 
 ##
-from nellix2ssdf import Nellix2ssdf
+from nellix.nellix2ssdf import Nellix2ssdf
 foo = Nellix2ssdf(dicom_basedir,ptcode,basedir)
 print('load, crop and convert to ssdf: done')
 
 ##
-from _do_registration_nellix import _Do_Registration_Nellix
+from nellix._do_registration_nellix import _Do_Registration_Nellix
 foo = _Do_Registration_Nellix(ptcode,basedir)
 #vol = foo.getVol()
 print('registration: done')
 
 ##
-from _do_segmentation_nellix import _Do_Segmentation
+from nellix._do_segmentation_nellix import _Do_Segmentation
 foo = _Do_Segmentation(ptcode,basedir)
 print('segmentation: done')
 
 ##
-from _select_stent_endpoints import _Select_Stent_Endpoints
+from nellix._select_stent_endpoints import _Select_Stent_Endpoints
 foo = _Select_Stent_Endpoints(ptcode,basedir)
 StartPoints = foo.StartPoints
 EndPoints = foo.EndPoints
@@ -39,7 +41,7 @@ StartPoints = [(126.7, 99.8, 64.3), (136.7, 97.8, 66.2), (120.4, 99.8, 60.7)]
 EndPoints = [(119.8, 85.2, 113.4), (128.7, 77.2, 113.7), (104.7, 97.0, 66.6)]
 
 
-from _test_centerline import _Test_Centerline
+from nellix._test_centerline import _Test_Centerline
 foo = _Test_Centerline(ptcode,StartPoints,EndPoints,basedir)
 allcenterlines = foo.allcenterlines # PointSet
 print('centerline: done')
@@ -47,12 +49,12 @@ print('centerline: done')
 ##
 # visualize centerlines and select points for motion comparison
 
-from _select_centerline_points import _Select_Centerline_Points
+from nellix._select_centerline_points import _Select_Centerline_Points
 foo = _Select_Centerline_Points(ptcode, allcenterlines, StartPoints,EndPoints,basedir)
 #foo = _Select_Centerline_Points(ptcode,basedir)
 
 ##
-from _show_model import _Show_Model
+from nellix._show_model import _Show_Model
 foo = _Show_Model(ptcode,basedir)
 print('chEVAS centerline finished')
 
