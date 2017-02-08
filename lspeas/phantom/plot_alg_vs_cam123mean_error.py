509ea22c7afc1bef3c6aec11351f483c17376a42
maaikekoenrades <maaikekoenrades@localhost>
2017-02-08 17:15:07 +0100

    add plot frequency patterns cam vs alg used spie proc

--- lspeas/phantom/plot_alg_vs_cam123mean_error.py
+++ lspeas/phantom/plot_alg_vs_cam123mean_error.py
@@ -35,13 +35,14 @@ exceldir = select_dir(r'C:\Users\Maaike\Dropbox\UTdrive\LSPEAS\Analysis\Validati
                       r'D:\Profiles\koenradesma\Dropbox\UTdrive\LSPEAS\Analysis\Validation robot')
 workbook = 'Errors cam123ref_vs_alg Toshiba.xlsx'
 dirsave =  select_dir(r'C:\Users\Maaike\Desktop','D:\Profiles\koenradesma\Desktop')
-
+savefig = False
 # plot frequency profiles
 # profiles, mean_abs_error, SD, MIN, Q1, Q3, MAX  = read_error_ouput(exceldir, workbookErrors)
 
 profilesB = ['ZA6', 'ZB1', 'ZB2', 'ZB3', 'ZB6', 'ZB5', 'ZB4']
-profilesA = [ 'ZA1', 'ZA2', 'ZA3', 'ZA3 STENT2']
-profilesBxaxis = [0, 0.23, 0.37,0.71, 1.19, 1.26, 1.36] 
+profilesA = [ 'ZA1', 'ZA2', 'ZA3']#, 'ZA3 STENT2']   # A1, 
+profilesBxaxis = [0.02, 0.23, 0.37,0.71, 1.19, 1.26, 1.36]
+profilesAxaxis = [49.0, 96.3, 73.3]  
 
 ## amplitude patterns
 abs_errors_profiles = read_error_cam123(exceldir, workbook, profilesB)
@@ -52,10 +53,10 @@ maxabserrorprofiles = [np.max(x) for x in abs_errors_profiles]
 q75abserrorprofiles = [np.percentile(x,75) for x in abs_errors_profiles]
 
 
-f1 = plt.figure(num=2, figsize=(7.6, 5))
+f1 = plt.figure(num=1, figsize=(7.6, 5))
 ax1 = f1.add_subplot(111)
 xlim = 1.45
-ylim = 0.7
+ylim = 0.65
 
 ax1.plot(profilesBxaxis, meanabserrorsprofiles, linestyle='r-', marker='.', color='b') 
 # ax1.plot(profilesBxaxis, maxabserrorprofiles, 'r--') # dotted line for min and max
@@ -76,8 +77,41 @@ plt.setp(bp['whiskers'], color='k', linestyle='-')
 plt.setp(bp['fliers'], color='k', marker='+')
 
 # save
-f1.savefig(os.path.join(dirsave, 'abserrorgraphampl2.pdf'), papertype='a0', dpi=300)
+if savefig:
+    f1.savefig(os.path.join(dirsave, 'abserrorgraphampl.pdf'), papertype='a0', dpi=300)
 
+
+## frequency patterns
+abs_errors_profiles = read_error_cam123(exceldir, workbook, profilesA)
+
+meanabserrorsprofiles = [np.mean(x) for x in abs_errors_profiles]
+stdabserrorprofiles = [np.std(x) for x in abs_errors_profiles]
+maxabserrorprofiles = [np.max(x) for x in abs_errors_profiles]
+q75abserrorprofiles = [np.percentile(x,75) for x in abs_errors_profiles]
+
+
+f1 = plt.figure(num=2, figsize=(7.6, 5))
+ax1 = f1.add_subplot(111)
+ylim = 0.65
+
+ax1.plot(profilesAxaxis, meanabserrorsprofiles, linestyle='r-', marker='.', color='b') 
+# ax1.plot(profilesAxaxis, maxabserrorprofiles, 'r--') # dotted line for min and max
+
+bp = plt.boxplot(abs_errors_profiles, positions=profilesAxaxis, widths=2.5)
+
+_initaxis([ax1], legend='upper right', xlabel='frequency of pattern (bpm)', 
+          ylabel='absolute error (mm)')
+major_ticks = np.arange(40, 100, 10)
+ax1.set_ylim((0, ylim))
+ax1.set_xlim(40,100)
+ax1.set_xticks(major_ticks)
+plt.setp(bp['boxes'], color='k')
+plt.setp(bp['whiskers'], color='k', linestyle='-')
+plt.setp(bp['fliers'], color='k', marker='+')
+
+# save
+if savefig:
+    f1.savefig(os.path.join(dirsave, 'abserrorgraphfreq.pdf'), papertype='a0', dpi=300)
 
 
 
