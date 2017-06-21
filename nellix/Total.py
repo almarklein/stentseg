43ff4035b4284da1270b845855451555dd285cdf
maaikekoenrades <maaikekoenrades@localhost>
2017-06-21 13:24:09 +0200

    nellix total add ctcode and reloading centerline model

--- nellix/Total.py
+++ nellix/Total.py
@@ -4,57 +4,65 @@ Created on 1 jun. 2016
 @author: TomLoonen / modified MaaikeKoenrades
 '''
 import os
-from stentseg.utils.datahandling import select_dir
+from stentseg.utils.datahandling import select_dir, loadmodel, loadvol
+
 
 ptcode = 'chevas_07'
+ctcode = '12months'
 dicom_basedir = r'F:\Nellix_chevas\CT_SSDF\no 7\STD00001\10phases'
 basedir = select_dir('E:/CT/SSDF/', r'F:\Nellix_chevas\CT_SSDF\SSDF')
 print(dicom_basedir)
 
-##
+
+## LOAD CT
 from nellix.nellix2ssdf import Nellix2ssdf
 foo = Nellix2ssdf(dicom_basedir,ptcode,basedir)
 print('load, crop and convert to ssdf: done')
 
-##
+## REGISTRATION
 from nellix._do_registration_nellix import _Do_Registration_Nellix
-foo = _Do_Registration_Nellix(ptcode,basedir)
+foo = _Do_Registration_Nellix(ptcode,ctcode,basedir)
 #vol = foo.getVol()
 print('registration: done')
 
-##
+## SEGEMENTATION
 from nellix._do_segmentation_nellix import _Do_Segmentation
-foo = _Do_Segmentation(ptcode,basedir)
+foo = _Do_Segmentation(ptcode,ctcode,basedir)
 print('segmentation: done')
 
-##
+## SELECT POINT FOR CENTERLINE
 from nellix._select_stent_endpoints import _Select_Stent_Endpoints
-foo = _Select_Stent_Endpoints(ptcode,basedir)
-StartPoints = foo.StartPoints
-EndPoints = foo.EndPoints
+foo = _Select_Stent_Endpoints(ptcode,ctcode,basedir)
+StartPoints = foo.StartPoints # proximal
+EndPoints = foo.EndPoints # distal
 print('Get Endpoints: done')
 
-##
-# # Points no 
+## CALCULATE CENTERLINE
 
-# StartPoints = [(126.7, 99.8, 64.3), (136.7, 97.8, 66.2), (120.4, 99.8, 60.7)]
-# EndPoints = [(119.8, 85.2, 113.4), (128.7, 77.2, 113.7), (104.7, 97.0, 66.6)]
+StartPoints = [[184.4, 111, 14.5], [188.4, 124.9, 17.4], [155.3, 113.5, 16.4]]
+EndPoints = [[173.3, 84.6, 109.7], [166.4, 94.7, 109.7], [146.9, 108.2, 34]]
 
 
 from nellix._get_centerline import _Get_Centerline
-foo = _Get_Centerline(ptcode,StartPoints,EndPoints,basedir)
+foo = _Get_Centerline(ptcode,ctcode,EndPoints,StartPoints,basedir) # MK: use endpoints distal as start!
 allcenterlines = foo.allcenterlines # PointSet
 print('centerline: done')
 
-##
-# visualize centerlines and select points for motion comparison
+## MOTION ANALYSIS CENTERLINES
+
+from stentseg.utils.centerline import points_from_nodes_in_graph
+s = loadmodel(basedir, ptcode, ctcode, 'prox', modelname='centerline_modelavgreg_deforms')
+allcenterlines = []
+for key in dir(s):
+        if key.startswith('model'):
+            model = points_from_nodes_in_graph(s[key])
+            allcenterlines.append(model)
 
 from nellix._select_centerline_points import _Select_Centerline_Points
-foo = _Select_Centerline_Points(ptcode, allcenterlines, StartPoints,EndPoints,basedir)
-#foo = _Select_Centerline_Points(ptcode,basedir)
+foo = _Select_Centerline_Points(ptcode, ctcode, allcenterlines, basedir)
 
-##
+## SHOW CENTERLINE DYNAMIC
 from nellix._show_model import _Show_Model
-foo = _Show_Model(ptcode,basedir)
+foo = _Show_Model(ptcode,ctcode,basedir)
 print('chEVAS centerline finished')
 
