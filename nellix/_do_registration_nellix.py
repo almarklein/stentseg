412ee5edcc2caf103fa01e06fbc177427293adb7
maaikekoenrades <maaikekoenrades@localhost>
2017-06-21 13:27:33 +0200

    remove redundant and check sampling storage

--- nellix/_do_registration_nellix.py
+++ nellix/_do_registration_nellix.py
@@ -1,11 +1,8 @@
 """ 
-Script to do the registation. This loads in N ecg-gated volumes and
-calculates N deformation fields from it, which are then stored to disk.
-We can also create an average image from all volumes by first registering
-the volumes toward each-other.
+Script to do the registation.
 """
 class _Do_Registration_Nellix:
-    def __init__(self,ptcode,basedir):
+    def __init__(self,ptcode,ctcode,basedir):
         ## Perform image registration
         
         import os, time 
@@ -22,7 +19,6 @@ class _Do_Registration_Nellix:
               
         # Select dataset to register
         cropname = 'prox'
-        ctcode = '12months'
         what = 'phases'
         
         # Load volumes
@@ -42,16 +38,14 @@ class _Do_Registration_Nellix:
                 vol_zoom_type = vv.Aarray(vol_zoom, s[key].sampling, s[key].origin)
                 vol = vol_zoom_type
                 
-                # vol = s[key]
                 phases.append(key)
                 vols.append(vol)
-                s[key] = vols # todo: maaike: ???? waarom s.vol90 alle vols uiteindelijk?
                 
         t0 = time.time()
         
         # Initialize registration object
         reg = pirt.reg.GravityRegistration(*vols)
-        #
+        
         reg.params.mass_transforms = 2  # 2nd order (Laplacian) triggers more at lines
         reg.params.speed_factor = 1.0
         reg.params.deform_wise = 'groupwise' # groupwise!
@@ -90,7 +84,7 @@ class _Do_Registration_Nellix:
             phase = phases[i][3:]
             s2['deform%s'%phase] = fields
         s2.sampling = s2['deform%s'%phase][0].sampling  # Sampling of deform is different!
-        s2.origin = s2.deform0[0].origin  # But origin is zero  # maaike: dit was een comment
+        s2.originDeforms = s2.deform0[0].origin  # But origin is zero
         s2.params = reg.params
         
         # Save
@@ -116,13 +110,11 @@ class _Do_Registration_Nellix:
         for key in dir(s):
             if key.startswith('meta'):
                 s_avg[key] = s[key]
-        #s_avg.sampling = vol.sampling  # z, y, x voxel size in mm ; last vol after interpolation
-        s_avg.sampling = [s.sampling[1], s.sampling[1], s.sampling[2]] # z, y, x voxel size in mm # Mirthe
+        s_avg.sampling = s.vol0.sampling # z, y, x after interpolation
         s_avg.origin = s.origin
         s_avg.stenttype = s.stenttype
         s_avg.croprange = s.croprange
         s_avg.vol = mean_vol.astype('float32')
-        #s_avg.vol.sampling = s_avg.sampling
         s_avg.params = s2.params
         
         fig1 = vv.figure(1); vv.clf()
@@ -134,27 +126,6 @@ class _Do_Registration_Nellix:
         #a1b.isoThreshold = 600
         vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
         vv.title('Average volume of %s phases (%s) (resized data)' % (len(phases), renderstyle))
-        
-        # a2 = vv.subplot(122)
-        # a2.daspect = 1, 1, -1
-        # renderstyle = 'mip'
-        # a2b = vv.volshow(s_avg.vol_original, clim=(0,2000), renderStyle = renderstyle)
-        # #a1b.isoThreshold = 600
-        # vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
-        # vv.title('Average volume of %s phases (%s) (original data)' % (len(phases), renderstyle))
-        # 
-        # a1.camera = a2.camera
-        
-        # fig = vv.figure(1); vv.clf()
-        # fig.position = 0, 22, 1366, 706
-        # a1 = vv.subplot(121)
-        # # t1 = vv.volshow(vol1, clim=(0, 3000), renderStyle='iso') # iso or mip
-        # # t1.isoThreshold = 600 # stond op 400 maar je moet hoger zetten als je alleen stent wil
-        # # t1.colormap = colormap
-        # a1b = vv.volshow2(vol1, clim=(-500, 1500),renderStyle='mip')
-        # vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
-        # # vv.title('One volume at %i procent of cardiac cycle' % phase )
-        # vv.title('Vol40' )
         
         # Save
         avg = 'avgreg'
