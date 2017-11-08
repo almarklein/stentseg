39c791162484a9cfa5b3fd624c447d884baee90d
maaikekoenrades <maaikekoenrades@localhost>
2017-11-08 10:16:01 +0100

    cam error add if tt1a plot programmed signal

--- lspeas/phantom/camera_error.py
+++ lspeas/phantom/camera_error.py
@@ -17,95 +17,94 @@ from lspeas.phantom.peakdetection import peakdet
 from lspeas.analysis.utils_analysis import _initaxis
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-if __name__ == '__main__':
-    
-    from stentseg.utils.datahandling import select_dir
-    
-    # preset dirs
-    dirsave =  select_dir(r'C:\Users\Maaike\Desktop','D:\Profiles\koenradesma\Desktop')
-    exceldir = select_dir(r'C:\Users\Maaike\Dropbox\UTdrive\LSPEAS\Analysis\Validation robot', 
-                  r'D:\Profiles\koenradesma\Dropbox\UTdrive\LSPEAS\Analysis\Validation robot')
-    workbookCam1 = 'Grafieken camera matlab meting 21012016.xlsx' # 21/1/2016
-    workbookCam2 = '20160215 GRAFIEKEN van camera systeem uit matlab in excel.xlsx' # 22/1/2016
-    workbookCam3 = 'Grafieken camera matlab meting 25012016.xlsx' # 25/1/2016
-    
-    sheetProfile = 'ZA1'
-    ylim = 1.5 # input + 0.45 marge legend
-    xlim = (-1.5,7)
-    colSt1 = 'D' # see workbookCams en Summery in Errors cam123ref_vs_alg Toshiba.xlsx
-    colSt2 = 'D'
-    colSt3 = 'D'
-    
-    # read the cam signal with consecutive periods
-    f1 = plt.figure(figsize=(18,11), num=1); plt.clf()
-    ax0 = f1.add_subplot(211)
-    
-    # read cam data
-    time_all_cam1, pos_all_cam1 = readCameraExcel(exceldir, workbookCam1, sheetProfile, colSt1)
-    time_all_cam2, pos_all_cam2 = readCameraExcel(exceldir, workbookCam2, sheetProfile, colSt2)
-    time_all_cam3, pos_all_cam3 = readCameraExcel(exceldir, workbookCam3, sheetProfile, colSt3)
-    
-    # make sure position min value is 0
-    pos_all_cam1 = np.asarray(pos_all_cam1) - min(pos_all_cam1)
-    pos_all_cam2 = np.asarray(pos_all_cam2) - min(pos_all_cam2)
-    pos_all_cam3 = np.asarray(pos_all_cam3) - min(pos_all_cam3)
-    
-    # get local minima as starting points for camera periods
-    peakmax1, peakmin1 = peakdet(pos_all_cam1, 0.05)
-    if sheetProfile == 'ZB5' or sheetProfile == 'ZB6':
-        peakmin1 = peakmin1[::2] # only for B5 and B6 with extra gauss peak
-    ttPeriodStarts1 = [time_all_cam1[int(peak)] for peak in peakmin1[:,0]] # get time in s
-    Tcam1 = (ttPeriodStarts1[-1]-ttPeriodStarts1[0])/(len(peakmin1)-1) # period of signal
-    ttPeriodPeaks1 = [time_all_cam1[int(peak)] for peak in peakmax1[:,0]] # get time in s
-    
-    peakmax2, peakmin2 = peakdet(pos_all_cam2, 0.05)
-    if sheetProfile == 'ZB5' or sheetProfile == 'ZB6':
-        peakmin2 = peakmin2[::2] # only for B5 and B6 with extra gauss peak
-    ttPeriodStarts2 = [time_all_cam2[int(peak)] for peak in peakmin2[:,0]] # get time in s
-    Tcam2 = (ttPeriodStarts2[-1]-ttPeriodStarts2[0])/(len(peakmin2)-1) # period of signal
-    ttPeriodPeaks2 = [time_all_cam2[int(peak)] for peak in peakmax2[:,0]] # get time in s
-    
-    peakmax3, peakmin3 = peakdet(pos_all_cam3, 0.05)
-    if sheetProfile == 'ZB5' or sheetProfile == 'ZB6':
-        peakmin3 = peakmin3[::2] # only for B5 and B6 with extra gauss peak
-    ttPeriodStarts3 = [time_all_cam3[int(peak)] for peak in peakmin3[:,0]] # get time in s
-    Tcam3 = (ttPeriodStarts3[-1]-ttPeriodStarts3[0])/(len(peakmin3)-1) # period of signal
-    ttPeriodPeaks3 = [time_all_cam3[int(peak)] for peak in peakmax3[:,0]] # get time in s
-    
-    # offset from t=0
-    offsett2 = ttPeriodStarts2[0] 
-    offsett1 = ttPeriodStarts1[0]
-    offsett3 = ttPeriodStarts3[0] 
-    
-    time_all_cam1 = np.asarray(time_all_cam1)
-    time_all_cam2 = np.asarray(time_all_cam2)
-    time_all_cam3 = np.asarray(time_all_cam3)
-    
-    ttPeriodStarts1 = np.asarray(ttPeriodStarts1)
-    ttPeriodStarts2 = np.asarray(ttPeriodStarts2)
-    ttPeriodStarts3 = np.asarray(ttPeriodStarts3)
-    ttPeriodPeaks1 = np.asarray(ttPeriodPeaks1)
-    ttPeriodPeaks2 = np.asarray(ttPeriodPeaks2)
-    ttPeriodPeaks3 = np.asarray(ttPeriodPeaks3)
-    
-    time_all_cam1t0 = time_all_cam1-offsett1
-    time_all_cam2t0 = time_all_cam2-offsett2
-    time_all_cam3t0 = time_all_cam3-offsett3
-    # plot cam signals and start points periods
-    ax0.plot(time_all_cam1t0, pos_all_cam1, 'r.-', alpha=0.5, label='camera reference 1')
-    ax0.scatter(ttPeriodStarts1-offsett1, np.array(peakmin1)[:,1], color='green')
-    ax0.scatter(ttPeriodPeaks1-offsett1, np.array(peakmax1)[:,1], color='k')
-    ax0.plot(time_all_cam2t0, pos_all_cam2, 'g.-', alpha=0.5, label='camera reference 2')
-    ax0.scatter(ttPeriodStarts2-offsett2, np.array(peakmin2)[:,1], color='green')
-    ax0.scatter(ttPeriodPeaks2-offsett2, np.array(peakmax2)[:,1], color='k')
-    ax0.plot(time_all_cam3t0, pos_all_cam3, 'b.-', alpha=0.5, label='camera reference 3')
-    ax0.scatter(ttPeriodStarts3-offsett3, np.array(peakmin3)[:,1], color='green')
-    ax0.scatter(ttPeriodPeaks3-offsett3, np.array(peakmax3)[:,1], color='k')
-    
-    _initaxis([ax0], legend='upper right', xlabel='time (s)', ylabel='position (mm)')
-    ax0.set_ylim((-0.02, ylim))
-    ax0.set_xlim(xlim)
+from stentseg.utils.datahandling import select_dir
+
+# preset dirs
+dirsave =  select_dir(r'C:\Users\Maaike\Desktop','D:\Profiles\koenradesma\Desktop')
+exceldir = select_dir(r'C:\Users\Maaike\Dropbox\UTdrive\LSPEAS\Analysis\Validation robot', 
+                r'D:\Profiles\koenradesma\Dropbox\UTdrive\LSPEAS\Analysis\Validation robot')
+workbookCam1 = 'Grafieken camera matlab meting 21012016.xlsx' # 21/1/2016
+workbookCam2 = '20160215 GRAFIEKEN van camera systeem uit matlab in excel.xlsx' # 22/1/2016
+workbookCam3 = 'Grafieken camera matlab meting 25012016.xlsx' # 25/1/2016
+
+sheetProfile = 'ZA1'
+ylim = 1.5 # input + 0.45 marge legend
+xlim = (-1.5,7)
+colSt1 = 'D' # see workbookCams en Summery in Errors cam123ref_vs_alg Toshiba.xlsx
+colSt2 = 'D'
+colSt3 = 'D'
+
+# read the cam signal with consecutive periods
+f1 = plt.figure(figsize=(18,11), num=1); plt.clf()
+ax0 = f1.add_subplot(211)
+
+# read cam data
+time_all_cam1, pos_all_cam1 = readCameraExcel(exceldir, workbookCam1, sheetProfile, colSt1)
+time_all_cam2, pos_all_cam2 = readCameraExcel(exceldir, workbookCam2, sheetProfile, colSt2)
+time_all_cam3, pos_all_cam3 = readCameraExcel(exceldir, workbookCam3, sheetProfile, colSt3)
+
+# make sure position min value is 0
+pos_all_cam1 = np.asarray(pos_all_cam1) - min(pos_all_cam1)
+pos_all_cam2 = np.asarray(pos_all_cam2) - min(pos_all_cam2)
+pos_all_cam3 = np.asarray(pos_all_cam3) - min(pos_all_cam3)
+
+# get local minima as starting points for camera periods
+peakmax1, peakmin1 = peakdet(pos_all_cam1, 0.05)
+if sheetProfile == 'ZB5' or sheetProfile == 'ZB6':
+    peakmin1 = peakmin1[::2] # only for B5 and B6 with extra gauss peak
+ttPeriodStarts1 = [time_all_cam1[int(peak)] for peak in peakmin1[:,0]] # get time in s
+Tcam1 = (ttPeriodStarts1[-1]-ttPeriodStarts1[0])/(len(peakmin1)-1) # period of signal
+ttPeriodPeaks1 = [time_all_cam1[int(peak)] for peak in peakmax1[:,0]] # get time in s
+
+peakmax2, peakmin2 = peakdet(pos_all_cam2, 0.05)
+if sheetProfile == 'ZB5' or sheetProfile == 'ZB6':
+    peakmin2 = peakmin2[::2] # only for B5 and B6 with extra gauss peak
+ttPeriodStarts2 = [time_all_cam2[int(peak)] for peak in peakmin2[:,0]] # get time in s
+Tcam2 = (ttPeriodStarts2[-1]-ttPeriodStarts2[0])/(len(peakmin2)-1) # period of signal
+ttPeriodPeaks2 = [time_all_cam2[int(peak)] for peak in peakmax2[:,0]] # get time in s
+
+peakmax3, peakmin3 = peakdet(pos_all_cam3, 0.05)
+if sheetProfile == 'ZB5' or sheetProfile == 'ZB6':
+    peakmin3 = peakmin3[::2] # only for B5 and B6 with extra gauss peak
+ttPeriodStarts3 = [time_all_cam3[int(peak)] for peak in peakmin3[:,0]] # get time in s
+Tcam3 = (ttPeriodStarts3[-1]-ttPeriodStarts3[0])/(len(peakmin3)-1) # period of signal
+ttPeriodPeaks3 = [time_all_cam3[int(peak)] for peak in peakmax3[:,0]] # get time in s
+
+# offset from t=0
+offsett2 = ttPeriodStarts2[0] 
+offsett1 = ttPeriodStarts1[0]
+offsett3 = ttPeriodStarts3[0] 
+
+time_all_cam1 = np.asarray(time_all_cam1)
+time_all_cam2 = np.asarray(time_all_cam2)
+time_all_cam3 = np.asarray(time_all_cam3)
+
+ttPeriodStarts1 = np.asarray(ttPeriodStarts1)
+ttPeriodStarts2 = np.asarray(ttPeriodStarts2)
+ttPeriodStarts3 = np.asarray(ttPeriodStarts3)
+ttPeriodPeaks1 = np.asarray(ttPeriodPeaks1)
+ttPeriodPeaks2 = np.asarray(ttPeriodPeaks2)
+ttPeriodPeaks3 = np.asarray(ttPeriodPeaks3)
+
+time_all_cam1t0 = time_all_cam1-offsett1
+time_all_cam2t0 = time_all_cam2-offsett2
+time_all_cam3t0 = time_all_cam3-offsett3
+# plot cam signals and start points periods
+ax0.plot(time_all_cam1t0, pos_all_cam1, 'r.-', alpha=0.5, label='camera reference 1')
+ax0.scatter(ttPeriodStarts1-offsett1, np.array(peakmin1)[:,1], color='green')
+ax0.scatter(ttPeriodPeaks1-offsett1, np.array(peakmax1)[:,1], color='k')
+ax0.plot(time_all_cam2t0, pos_all_cam2, 'g.-', alpha=0.5, label='camera reference 2')
+ax0.scatter(ttPeriodStarts2-offsett2, np.array(peakmin2)[:,1], color='green')
+ax0.scatter(ttPeriodPeaks2-offsett2, np.array(peakmax2)[:,1], color='k')
+ax0.plot(time_all_cam3t0, pos_all_cam3, 'b.-', alpha=0.5, label='camera reference 3')
+ax0.scatter(ttPeriodStarts3-offsett3, np.array(peakmin3)[:,1], color='green')
+ax0.scatter(ttPeriodPeaks3-offsett3, np.array(peakmax3)[:,1], color='k')
+
+_initaxis([ax0], legend='upper right', xlabel='time (s)', ylabel='position (mm)')
+ax0.set_ylim((-0.02, ylim))
+ax0.set_xlim(xlim)
     
 
 ##  overlay signals with consecutive periods, smallest rmse
@@ -573,7 +572,7 @@ def repeatCamPeriod(tt,aa, aastd=None, mark=False, correct0=True, **kwargs):
 
 def show_period_cam123bestCut_with_bounds(ttperiodmeanC1, ttperiodmeanC2, 
         ttperiodmeanC3, ttperiodmeanC123, pperiodsC1bestCut, pperiodsC2bestCut, 
-        pperiodsC3bestCut, pperiodsC123bestCut, fignum=4, save=True):
+        pperiodsC3bestCut, pperiodsC123bestCut, tt1a=None, aa1a=None, fignum=4, save=True):
     
     from stentseg.utils.aortamotionpattern import get_motion_pattern, plot_pattern_plt
     
@@ -599,7 +598,8 @@ def show_period_cam123bestCut_with_bounds(ttperiodmeanC1, ttperiodmeanC2,
                                         pperiodsC3mean,pperiodsC3std)
     
     # add input function simulator; first run gauspatterns.py
-    plot_pattern_plt(*(tt1a,aa1a),label='input A1', ls='--', mark=False)
+    if tt1a:
+        plot_pattern_plt(*(tt1a,aa1a),label='input A1', ls='--', mark=False)
     
     colors = ['#d7191c','#fdae61','#2c7bb6'] # http://colorbrewer2.org/#type=diverging&scheme=RdYlBu&n=5
     ax2.plot(ttperiodmeanC1rep, pperiodsC1mean, '.-', color=colors[0], label='output day 1 (camera)')
@@ -625,7 +625,8 @@ def show_period_cam123bestCut_with_bounds(ttperiodmeanC1, ttperiodmeanC2,
     # add plot of average with bounds
     ax3 = f3.add_subplot(122)
     # add input function simulator
-    plot_pattern_plt(*(tt1a,aa1a),label='input A1', ls='--') # (A1: A=1.0, T=1.2)')
+    if tt1a:
+        plot_pattern_plt(*(tt1a,aa1a),label='input A1', ls='--') # (A1: A=1.0, T=1.2)')
     
     ttperiodmeanC123rep, pperiodsC123meanRep, pperiodsC123stdRep = repeatCamPeriod(ttperiodmeanC123,
                                     pperiodsC123mean,pperiodsC123std, mark=True)
