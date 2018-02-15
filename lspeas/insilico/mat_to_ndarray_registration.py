6e89a22daa399872b64356c9c56ff5baa81a2107
maaikekoenrades <maaikekoenrades@localhost>
2018-02-15 11:34:45 +0100

    subplot and title different

--- lspeas/insilico/mat_to_ndarray_registration.py
+++ lspeas/insilico/mat_to_ndarray_registration.py
@@ -77,109 +77,112 @@ if False: # get a slice not vol
 f = vv.figure(1); vv.clf()
 f.position = 0.00, 22.00,  1914.00, 1018.00
 clim = (0,1000)
-a1 = vv.subplot(131)
+a1 = vv.subplot(221)
 vv.volshow(volOr,clim=clim)
 vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
-vv.title('Im1-Original')
+vv.title('Im1:Original')
 
-a2 = vv.subplot(132)
+a2 = vv.subplot(222)
 vv.volshow(volTr,clim=clim)
 vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
-vv.title('Im2-Original transformed')
+vv.title('Im2:Original transformed')
 
-a3 = vv.subplot(133)
+a3 = vv.subplot(223)
 a3.daspect = 1, 1, -1
 vv.volshow(volOr,clim=clim)
 vv.volshow(volTr,clim=clim)
 vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
 vv.title('Overlay')
 
-a1.camera = a2.camera = a3.camera
+a4 = vv.subplot(224)
+a1.camera = a2.camera = a3.camera = a4.camera
 
 ## Do registration
-t0 = time.time()
 
-# Initialize registration object
-reg = pirt.reg.GravityRegistration(*vols)
-# Set params
-reg.params.mass_transforms = 2  # 2;2nd order (Laplacian) triggers more at lines
-reg.params.deform_wise = 'groupwise' # groupwise!
-reg.params.mapping = 'backward'
-reg.params.deform_limit = 1.0
-reg.params.final_scale = 1.0  #1.0 We might set this a wee bit lower than 1 (but slower!)
-reg.params.grid_sampling_factor = 0.5 # 0.5 !! important especially for Laplace !!
-# most important params
-reg.params.speed_factor = 1.0 # ch9 0.5-2.0; ch10 2.0
-reg.params.scale_sampling = 16 # 16; nr of iterations per scale level (ch10 16-200)
-reg.params.final_grid_sampling = 20 #20; grid sampling ch9/10: 5-15-30/20 
-                                    #This parameter is usually best coupled to final_scale
-
-# Go!
-reg.register(verbose=1)
-
-# t1 = time.time()
-# print('Registration completed, which took %1.2f min.' % ((t1-t0)/60))
-
-
-# Store registration result
-
-from visvis import ssdf
-
-# Create struct
-
-s2 = vv.ssdf.new()
-N = len(vols)
-s2.meta = s['meta'+orivol] # from orig vol
-s2.origin = s.origin
-s2.stenttype = s.stenttype
-s2.croprange = s.croprange
-
-# Obtain deform fields
-for i in range(N):
-    fields = [field for field in reg.get_deform(i).as_backward()] # fields of z,y,x
-    s2['deform%i'%i] = fields
-s2.sampling = s2.deform0[0].sampling  # Sampling of deform is different!
-s2.originDeforms = s2.deform0[0].origin  # But origin is zero
-s2.params = reg.params
-
-# Save
-name = cropname+fileOr
-filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, name, 'deforms')
-ssdf.save(os.path.join(dirsaveinsilico, ptcode, filename), s2)
-print("deforms saved to disk.")
-
-
-# Store averaged volume, where the volumes are registered
-
-# Create average volume from *all* volumes deformed to the "center"
-N = len(reg._ims)
-mean_vol = np.zeros(reg._ims[0].shape, 'float64')
-for i in range(N):
-    vol, deform = reg._ims[i], reg.get_deform(i) # reg._ims[0]==volOr
-    mean_vol += deform.as_backward().apply_deformation(vol)
-mean_vol *= 1.0/N
-
-# Create struct
-s_avg = ssdf.new()
-s_avg.meta = s['meta'+orivol]
-s_avg.sampling = sampling2 # z, y, x 
-s_avg.origin = origin2
-s_avg.samplingOriginal = sampling
-s_avg.originOriginal = origin
-s_avg.stenttype = s.stenttype
-s_avg.croprange = s.croprange
-s_avg.vol = mean_vol.astype('float32')
-s_avg.params = s2.params
-
-# Save
-avg = 'avgreg'
-filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, name, avg)
-ssdf.save(os.path.join(dirsaveinsilico, ptcode, filename), s_avg)
-print("avgreg saved to disk.")
-
-t1 = time.time()
-t_in_min = (t1-t0)/60
-print('Registration completed for %s - %s, which took %1.2f min.' % (ptcode,ctcode, t_in_min))
+if False:
+    t0 = time.time()
+    
+    # Initialize registration object
+    reg = pirt.reg.GravityRegistration(*vols)
+    # Set params
+    reg.params.mass_transforms = 2  # 2;2nd order (Laplacian) triggers more at lines
+    reg.params.deform_wise = 'groupwise' # groupwise!
+    reg.params.mapping = 'backward'
+    reg.params.deform_limit = 1.0
+    reg.params.final_scale = 1.0  #1.0 We might set this a wee bit lower than 1 (but slower!)
+    reg.params.grid_sampling_factor = 0.5 # 0.5 !! important especially for Laplace !!
+    # most important params
+    reg.params.speed_factor = 1.0 # ch9 0.5-2.0; ch10 2.0
+    reg.params.scale_sampling = 16 # 16; nr of iterations per scale level (ch10 16-200)
+    reg.params.final_grid_sampling = 20 #20; grid sampling ch9/10: 5-15-30/20 
+                                        #This parameter is usually best coupled to final_scale
+    
+    # Go!
+    reg.register(verbose=1)
+    
+    # t1 = time.time()
+    # print('Registration completed, which took %1.2f min.' % ((t1-t0)/60))
+    
+    
+    # Store registration result
+    
+    from visvis import ssdf
+    
+    # Create struct
+    
+    s2 = vv.ssdf.new()
+    N = len(vols)
+    s2.meta = s['meta'+orivol] # from orig vol
+    s2.origin = s.origin
+    s2.stenttype = s.stenttype
+    s2.croprange = s.croprange
+    
+    # Obtain deform fields
+    for i in range(N):
+        fields = [field for field in reg.get_deform(i).as_backward()] # fields of z,y,x
+        s2['deform%i'%i] = fields
+    s2.sampling = s2.deform0[0].sampling  # Sampling of deform is different!
+    s2.originDeforms = s2.deform0[0].origin  # But origin is zero
+    s2.params = reg.params
+    
+    # Save
+    name = cropname+fileOr
+    filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, name, 'deforms')
+    ssdf.save(os.path.join(dirsaveinsilico, ptcode, filename), s2)
+    print("deforms saved to disk.")
+    
+    
+    # Store averaged volume, where the volumes are registered
+    
+    # Create average volume from *all* volumes deformed to the "center"
+    N = len(reg._ims)
+    mean_vol = np.zeros(reg._ims[0].shape, 'float64')
+    for i in range(N):
+        vol, deform = reg._ims[i], reg.get_deform(i) # reg._ims[0]==volOr
+        mean_vol += deform.as_backward().apply_deformation(vol)
+    mean_vol *= 1.0/N
+    
+    # Create struct
+    s_avg = ssdf.new()
+    s_avg.meta = s['meta'+orivol]
+    s_avg.sampling = sampling2 # z, y, x 
+    s_avg.origin = origin2
+    s_avg.samplingOriginal = sampling
+    s_avg.originOriginal = origin
+    s_avg.stenttype = s.stenttype
+    s_avg.croprange = s.croprange
+    s_avg.vol = mean_vol.astype('float32')
+    s_avg.params = s2.params
+    
+    # Save
+    avg = 'avgreg'
+    filename = '%s_%s_%s_%s.ssdf' % (ptcode, ctcode, name, avg)
+    ssdf.save(os.path.join(dirsaveinsilico, ptcode, filename), s_avg)
+    print("avgreg saved to disk.")
+    
+    t1 = time.time()
+    t_in_min = (t1-t0)/60
+    print('Registration completed for %s - %s, which took %1.2f min.' % (ptcode,ctcode, t_in_min))
 
 
 ## Load deforms and apply deform to registered phases/vols
@@ -188,10 +191,11 @@ name = cropname+fileOr
 savg = loadvol(dirsaveinsilico, ptcode, ctcode, name, 'avgreg')
 vol = savg.vol
 
-f = vv.figure(2); vv.clf()
-f.position = 968.00, 30.00,  944.00, 1002.00
-a = vv.subplot(111)
-a.daspect = 1, 1, -1
+#f = vv.figure(2); vv.clf()
+#f.position = 968.00, 30.00,  944.00, 1002.00
+#a = vv.subplot(111)
+#a.daspect = 1, 1, -1
+a4.MakeCurrent()
 if False:
     t = vv.imshow(vol, clim=(0,2000))
 else:
