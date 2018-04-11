6f46ee18cce2b31ccb3d8c9504c529cf41fb3e57
maaikekoenrades <maaikekoenrades@localhost>
2018-04-11 22:52:34 +0200

    Total with test folder pt 05 and do automated analysis

--- nellix/Total.py
+++ nellix/Total.py
@@ -38,13 +38,15 @@ EndPoints = foo.EndPoints # distal
 print('Get Endpoints: done')
 
 ## CALCULATE CENTERLINE
-# 7
-# StartPoints = [[184.4, 111, 14.5], [188.4, 124.9, 17.4], [155.3, 113.5, 16.4]]
-# EndPoints = [[173.3, 84.6, 109.7], [166.4, 94.7, 109.7], [146.9, 108.2, 34]]
-
+# 7 -mirthe 2
+# StartPoints = [(184.3, 111.4, 16.7), (188.6, 125.2, 19.1), (154.9, 113.9, 17.8)] # prox
+# EndPoints = [(186.1, 90.0, 78.9), (188.3, 101.7, 78.9), (147.0, 107.8, 33.3)] # dist
+# 5 -mirthe 2
+StartPoints = [(122.4, 139.3, 31.3), (132.8, 137.2, 31.3), (126.9, 150.4, 33.5)] # prox
+EndPoints = [(123.9, 122.1, 69.5),(136.3, 125.1, 69.0), (147,130.8, 53.3) ]  # dist
 
 from nellix._get_centerline import _Get_Centerline
-foo = _Get_Centerline(ptcode,ctcode,EndPoints,StartPoints,basedir) # MK: endpoints here is distal points!
+foo = _Get_Centerline(ptcode,ctcode,EndPoints,StartPoints,basedir) # MK: start=distal points --> endpoints
 # loads prox_avgreg (reg) and prox_modelavgreg (segm)
 # saves centerline_modelavgreg and centerline_total_modelavgreg; also with _deforms as dynamic centerline
 # in centerline we have all centerlines but separate graphs; in centerline_total merged in one graph
@@ -63,15 +65,28 @@ a, s2 = identifyCenterlines(s, ptcode,ctcode,basedir,modelname,showVol='MIP')
 ## MOTION ANALYSIS CENTERLINES
 import os
 from stentseg.utils.datahandling import select_dir, loadmodel, loadvol
+from stentseg.utils.picker import pick3d
 
-ptcode = 'chevas_06'
+ptcode = 'chevas_05'
 ctcode = '12months'
-basedir = select_dir(r'F:\Nellix_chevas\CT_SSDF\SSDF')
+# basedir = select_dir(r'F:\Nellix_chevas\CT_SSDF\SSDF')
+basedir = select_dir(r'D:\Profiles\koenradesma\SURFdrive\Nellix_chevas_data_for_testing',
+                     r'C:\Users\Maaike\SURFdrive\Nellix_chevas_data_for_testing')
+
+s = loadmodel(basedir, ptcode, ctcode, 'prox', modelname='centerline_modelavgreg_deforms_id')
+# s4 = loadvol(basedir, ptcode, ctcode, 'prox', 'avgreg')
+# t = show_ctvolume(s4.vol, showVol='MIP', clim =(0,2500), isoTh=250,removeStent=False, climEditor=True)
+# label = pick3d(vv.gca(), s4.vol)
 
 from nellix._do_analysis_centerline import _Do_Analysis_Centerline
-dAC = _Do_Analysis_Centerline(ptcode,ctcode,basedir) # loads centerline_modelavgreg_deforms_id
-dAC.chimney_angle_change()
-##
+AC = _Do_Analysis_Centerline(ptcode,ctcode,basedir,showVol='MIP') # loads centerline_modelavgreg_deforms_id
+AC.chimneys_angle_change()
+AC.motion_centerlines_segments(lenSegment=5, dim='xyz')
+AC.distance_change_nelnel_nelCh()
+AC.storeOutputToExcel()
+
+## [OLD CODE] MOTION ANALYSIS CENTERLINES - CLICK NODES
+s = loadmodel(basedir, ptcode, ctcode, 'prox', modelname='centerline_modelavgreg_deforms')
 allcenterlines = []
 for key in dir(s):
         if key.startswith('model'):
