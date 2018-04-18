15551b2ddd202cde206ba374ce8a1d5d7b177e05
maaikekoenrades <maaikekoenrades@localhost>
2018-04-18 13:16:54 +0200

    get also 2 second point for endpoint centerline

--- lspeas/legs/_get_label_coordinates.py
+++ lspeas/legs/_get_label_coordinates.py
@@ -9,11 +9,30 @@ n1coordinates = np.asarray(label2worldcoordinates(label1),dtype=np.float32) # x,
 
 n2coordinates = np.asarray(label2worldcoordinates(label1),dtype=np.float32) # x,y,z
 
-## midpoint
+
+
+
+## point start
 
 start1 = (n1coordinates+n2coordinates)/2
 start1 = tuple(start1.flat)
 
-## vis midpoint
+## vis start point
+view = a0.GetView()
+p1 = vv.plot(start1[0], start1[1], start1[2], ms='.', ls='', mc='g', mw=15, axes=a0) # start1
+a0.SetView(view)
 
-vv.plot(start1[0], start1[1], start1[2], ms='.', ls='', mc='g', mw=15, axes=a0) # start1
+
+
+
+## point end
+
+end1 = (n1coordinates+n2coordinates)/2
+end1 = tuple(end1.flat)
+
+## vis end point
+
+view = a0.GetView()
+vv.plot(end1[0], end1[1], end1[2], ms='.', ls='', mc='r', mw=15, axes=a0) # start1
+a0.SetView(view)
+
