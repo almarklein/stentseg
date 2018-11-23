c1dac19cf698706d8361c273c4baa6cf5c0e9753
maaikekoenrades <maaikekoenrades@localhost>
2018-11-23 13:05:51 +0100

    add script to read dicom directly from a given location option to check in 4D by showVolPhases

---
+++ lspeas/_read_dicom.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+import imageio
+
+from stentseg.utils.datahandling import select_dir, loadvol
+from stentseg.utils.datahandling import savecropvols, saveaveraged, cropaveraged
+
+dicom_basedir = r'D:\Profiles\koenradesma\Desktop\LSPEASF_A_03c_arterieel2'
+vols2 = [vol2 for vol2 in imageio.get_reader(dicom_basedir, 'DICOM', 'V')]
+
+for i, vol in enumerate(vols2):
+    print(vol.meta.ImagePositionPatient)
+for i, vol in enumerate(vols2):
+    print(vol.shape)
+for i, vol in enumerate(vols2):
+    print(vol.meta.AcquisitionTime)
+    print(vol.meta.sampling)
+    assert vol.shape == vols2[0].shape
+    assert vol.meta.SeriesTime == vols2[0].meta.SeriesTime
+    
+
+## Test show vol
+from lspeas.utils.vis import showVolPhases
+
+showVol='mip'
+t = showVolPhases(dicom_basedir, vols2, showVol=showVol, 
+        mipIsocolor=True, isoTh=310, clim=(60,3000), slider=True  )
