1dc66ad53522071c3ea4e66972b1753219febc8b
maaikekoenrades <maaikekoenrades@localhost>
2019-08-05 11:41:33 +0200

    version 22-2-2019 (copy from folder geurts translation) automated reading mat files from velocityfieldtransfomration

--- lspeas/insilico/registration_mat_transformations.py
+++ lspeas/insilico/registration_mat_transformations.py
@@ -1,3 +1,8 @@
+""" Script to register mat file image volumes that were obtained from 
+get_and_deform_CTdata_loop.m
+
+2018-2019 Maaike Koenrades
+"""
 import scipy.io 
 import os
 import os, time
@@ -22,8 +27,8 @@ oriPhase = '50'
 #s = loadvol(basedir, ptcode, ctcode, cropname, what)
 sampling = (0.5, 0.383, 0.383) #s['vol'+oriPhase].sampling # z,y,x
 origin = [0, 34.087, 71.238] #s.origin
-sampling2 = (1,1,1) # ground truth sampling from Matlab (width_pixel=1)
-#todo: sample vol with dicom sampling for realistic registration?
+# sampling2 = (1,1,1) # ground truth sampling from Matlab (width_pixel=1)
+#todo: in matlab set pixel_width DataDressed/DataDressedInterpolated with dicom sampling for realistic registration?!
 origin2 = (0,0,0)
 
 # ======================================================
@@ -34,35 +39,95 @@ reg2d = False
 # Load the mat files with original and deformed volumes
 dirinsilico = select_dir(
         #r'D:\LSPEAS\LSPEAS_insilico_mat',
-        r'F:\LSPEAS_insilico_mat',
-        r'E:\LSPEAS_insilico_mat',
-        r'/Users/geurtsbj/ownCloud/research/articles/maaike/in.silico.validation/LSPEAS_insilico_mat')
+        #r'F:\LSPEAS_insilico_mat',
+        (r'D:\Profiles\koenradesma\SURFdrive\UTdrive\LSPEAS\In silico validation'
+            r'\translation_Geurts_2019-02-08\translation\LSPEAS_insilico_mat'),
+        (r'C:\Users\Maaike\SURFdrive\UTdrive\LSPEAS\In silico validation'
+            r'\translation_Geurts_2019-02-08\translation\LSPEAS_insilico_mat'),
+        #r'/Users/geurtsbj/ownCloud/research/articles/maaike/registration_sensitivity/registration_assessment/LSPEAS_insilico_mat',
+        r'/Users/geurtsbj/ownCloud/research/articles/maaike/registration_sensitivity_cases/translation/LSPEAS_insilico_mat')
 
 dirsaveinsilico = select_dir(
         #r'D:\LSPEAS\LSPEAS_insilico_ssdf\reg1',
-        r'F:\LSPEAS_insilico_ssdf\reg1',
-        r'E:\LSPEAS_insilico_ssdf\reg1',
-        r'/Users/geurtsbj/ownCloud/research/articles/maaike/in.silico.validation/LSPEAS_insilico_ssdf/reg1')
+        #r'F:\LSPEAS_insilico_ssdf\reg1',
+        (r'D:\Profiles\koenradesma\SURFdrive\UTdrive\LSPEAS\In silico validation'
+            r'\translation_Geurts_2019-02-08\translation\LSPEAS_insilico_ssdf\reg1'),
+        (r'C:\Users\Maaike\SURFdrive\UTdrive\LSPEAS\In silico validation'
+            r'\translation_Geurts_2019-02-08\translation\LSPEAS_insilico_ssdf\reg1'),
+        #r'/Users/geurtsbj/ownCloud/research/articles/maaike/registration_sensitivity/registration_assessment/LSPEAS_insilico_ssdf/reg1',
+        r'/Users/geurtsbj/ownCloud/research/articles/maaike/registration_sensitivity_cases/translation/LSPEAS_insilico_ssdf/reg1')
 
-fileOr = 'original_DataDressed_001_d50_1-146'
-fileTrs = [ 'tr_DataDressed_001_d50_1-146_1_-0.50pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_-1.00pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_-2.00pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_-3.00pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_-0.25pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_-0.10pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_0.00pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_0.10pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_0.25pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_0.50pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_1.00pi_t0pi_p0.5pi',
-            'tr_DataDressed_001_d50_1-146_1_2.00pi_t0pi_p0.5pi'
-            ]
+# Get .mat volumes DataDressed and all DataDressedInterpolated automatically from folder dirinsilico\ptcode
+fileTrs = []
+files_in_folder = os.listdir(os.path.join(dirinsilico, ptcode))
+for file_name in files_in_folder:
+    if file_name.startswith('original'):
+        fileOr = file_name
+    elif file_name.startswith('tr_DD'): # collect all DataDressedInterpolated
+        fileTrs.append(file_name)
+
+# Or set manual filenames for volumes to register
+# fileOr = 'original_DataDressed_001_d50_1-146'
+# 
+# fileTrs =  [ 'tr_DD_001_d50_1-146_1_0.01_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.02_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.05_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.10_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.20_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.50_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_1.00_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_2.00_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_5.00_t0.25pi_p0.5pi', 
+#              'tr_DD_001_d50_1-146_1_10.00_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_20.00_t0.25pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_50.00_t0.25pi_p0.5pi'                  
+#              ]
+             
+# fileTrs =  [ 'tr_DD_001_d50_1-146_1_0.01_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.02_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.05_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.10_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.20_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.50_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_1.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_2.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_5.00_t0pi_p0.5pi', 
+#              'tr_DD_001_d50_1-146_1_10.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_20.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_50.00_t0pi_p0.5pi'                  
+#              ]
+#              
+# fileTrs =  [ 'tr_DD_001_d50_1-146_1_-0.01_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-0.02_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-0.05_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-0.10_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-0.20_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-0.50_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-1.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-2.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-5.00_t0pi_p0.5pi', 
+#              'tr_DD_001_d50_1-146_1_-10.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-20.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_-50.00_t0pi_p0.5pi', 
+#              'tr_DD_001_d50_1-146_1_0.01_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.02_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.05_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.10_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.20_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_0.50_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_1.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_2.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_5.00_t0pi_p0.5pi', 
+#              'tr_DD_001_d50_1-146_1_10.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_20.00_t0pi_p0.5pi',
+#              'tr_DD_001_d50_1-146_1_50.00_t0pi_p0.5pi'                  
+#              ]
 # ======================================================
 
+# load DataDressed and DataDressedInterpolated for all VelocityFieldTransformations
 for fileTr in fileTrs:
-    mat = scipy.io.loadmat(os.path.join(dirinsilico, ptcode, fileOr+'.mat'))
-    mat2 = scipy.io.loadmat(os.path.join(dirinsilico, ptcode, fileTr+'.mat'))
+    mat = scipy.io.loadmat(os.path.join(dirinsilico, ptcode, fileOr))
+    mat2 = scipy.io.loadmat(os.path.join(dirinsilico, ptcode, fileTr))
     
     volOr = mat['DataDressed']
     volTr = mat2['DataDressedInterpolated']
@@ -78,7 +143,7 @@ for fileTr in fileTrs:
         xmin, xmax = 300, -150
         # ======================
         croprange = [[0,volOr.shape[0]], [ymin,ymax], [xmin,xmax]] # zyx
-        croprangeMat = [[ymin+1,volOr.shape[1]+ymax],[xmin+1,volOr.shape[2]+xmax], [1,volOr.shape[0]]] # yxz
+        croprangeMat = [[ymin+1,volOr.shape[1]+ymax],[xmin+1,volOr.shape[2]+xmax], [1,volOr.shape[0]]] # yxz (x and y different direction)
         volOr = volOr[:, ymin:ymax, xmin:xmax]
         volTr = volTr[:, ymin:ymax, xmin:xmax]
     elif False: # crop z
@@ -96,6 +161,12 @@ for fileTr in fileTrs:
     if True: 
         volOr[volOr<-1000] = -200
         volTr[volTr<-1000] = -200
+    
+    # Get the sampling of DataDressed and DataDressedInterpolated
+    sampling2_x = mat['DressedVars']['width_pixel_y'][0][0][0][0] # y in matlab = x in python (left-right)
+    sampling2_y = mat['DressedVars']['width_pixel_x'][0][0][0][0] # x in matlab  = y in python (ant-post)
+    sampling2_z = mat['DressedVars']['width_pixel_z'][0][0][0][0]
+    sampling2 = tuple([sampling2_z,sampling2_y,sampling2_x]) # zyx
     
     # create Aarray volume    
     volOr = vv.Aarray(volOr, sampling2, origin2)
@@ -116,7 +187,7 @@ for fileTr in fileTrs:
         a1 = vv.subplot(221)
         vv.volshow(volOr,clim=clim)
         vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
-        vv.title('Im1:Original')
+        vv.title('Im1:Original (DataDressed)')
         a1.daspect = 1,1,-1
         
         a2 = vv.subplot(222)
@@ -129,7 +200,7 @@ for fileTr in fileTrs:
         a3 = vv.subplot(223)
         vv.volshow(volTr,clim=clim)
         vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
-        vv.title('Im2:Original transformed')
+        vv.title('Im2:Original transformed (DataDressedInterpolated u_\magnitude {})'.format(mat2['u_magnitude'][0][0]))
         a3.daspect = 1,1,-1
         
         a4 = vv.subplot(224)
@@ -139,10 +210,20 @@ for fileTr in fileTrs:
         
         a1.camera = a2.camera = a3.camera = a4.camera
     
+    # Remove .mat for savename and loading below
+    if fileTr.endswith('.mat'):
+        fileTr = fileTr.replace('.mat', '')
+    
     ## Do registration
     
     if True:
         t0 = time.time()
+        
+        # set final scale based on pixelwidth (scale should be =<0.5*pixelwidth)
+        if max(sampling2) > 2:
+            final_scale = max(sampling2)/2
+        else:
+            final_scale = 1.0
         
         # Initialize registration object
         reg = pirt.reg.GravityRegistration(*vols)
@@ -152,7 +233,7 @@ for fileTr in fileTrs:
         reg.params.deform_wise = 'groupwise' # groupwise!
         reg.params.mapping = 'backward'
         reg.params.deform_limit = 1.0
-        reg.params.final_scale = 1.0  #1.0 We might set this a wee bit lower than 1 (but slower!)
+        reg.params.final_scale = final_scale  #1.0 We might set this a wee bit lower than 1 (but slower!)
         reg.params.grid_sampling_factor = 0.5 # 0.5 !! important especially for Laplace !!
         # most important params
         reg.params.speed_factor = 1.0 # ch9 0.5-2.0; ch10 2.0
@@ -166,11 +247,9 @@ for fileTr in fileTrs:
         
         
         # Store registration result
-        
         from visvis import ssdf
         
         # Create struct
-        
         s2 = vv.ssdf.new()
         N = len(vols)
         # s2.meta = s['meta'+oriPhase] # from orig vol
@@ -193,8 +272,7 @@ for fileTr in fileTrs:
         
         
         # Store averaged volume, where the volumes are registered
-        
-        # Create average volume from *all* volumes deformed to the "center"
+        # create average volume from *all* volumes deformed to the "center"
         N = len(reg._ims)
         mean_vol = np.zeros(reg._ims[0].shape, 'float64')
         for i in range(N):
@@ -280,46 +358,49 @@ for fileTr in fileTrs:
     displacementOrToTr = mat2.copy()
     del displacementOrToTr['DataDressedInterpolated'] # remove Image data
     displacementOrToTr['displacementOrToTr_z'] = fieldOrBack_d_z
-    displacementOrToTr['displacementOrToTr_y'] = fieldOrBack_d_y
-    displacementOrToTr['displacementOrToTr_x'] = fieldOrBack_d_x
+    displacementOrToTr['displacementOrToTr_x'] = fieldOrBack_d_y # x and y direction different in matlab
+    displacementOrToTr['displacementOrToTr_y'] = fieldOrBack_d_x # x and y direction different in matlab
     displacementOrToTr['croprange_zyx'] = croprange
     displacementOrToTr['croprangeMat_yxz'] = croprangeMat
     displacementOrToTr['samplingOriginal'] = sampling
     displacementOrToTr['originOriginal'] = origin
+    displacementOrToTr['sampling'] = sampling2
+    displacementOrToTr['origin'] = origin2
+    
     
     # save mat file displacement
     if savemat:
-        storemat = os.path.join(dirsaveinsilico, ptcode, 'dispOrToTr_'+fileTr+'.mat')
+        storemat = os.path.join(dirsaveinsilico, ptcode, 'dispOrToTr_'+fileTr)
         scipy.io.savemat(storemat,displacementOrToTr)
         print('displacementOrToTr was stored to {}'.format(storemat))
     
     # visualize
     if visualize:
-        vv.figure(4); vv.clf()
+        vv.figure(2); vv.clf()
         a1b = vv.subplot(111)
         t1 = vv.volshow(volOr)
-        vv.title('original im 1 (Or)')
+        vv.title('original im 1 (DataDressed)')
         vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
         a1b.daspect = 1, 1, -1
         
-        vv.figure(5); vv.clf()
+        vv.figure(3); vv.clf()
         a2b = vv.subplot(111)
-        t2 = vv.volshow(volTrBack)
-        vv.title('im2 (tr) deformed to im1 (or)')
+        t2 = vv.volshow(volTrBack) # im1 as was calculated by the registration
+        vv.title('im2 (DataDressedInterpolated) transformed by registration to im1 (DataDressed)')
         vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
         a2b.daspect = 1, 1, -1
         
-        vv.figure(6); vv.clf()
+        vv.figure(4); vv.clf()
         a3b = vv.subplot(111)
         t3 = vv.volshow(volTr)
-        vv.title('original im2 (Tr)')
+        vv.title('original im2 (DataDressedInterpolated)')
         vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
         a3b.daspect = 1, 1, -1
         
-        vv.figure(7); vv.clf()
+        vv.figure(5); vv.clf()
         a4b = vv.subplot(111)
         t4 = vv.volshow(volOr-volTrBack)
-        vv.title('difference im1 (or) and im1 by registration')
+        vv.title('difference im1 (DataDressed) and im1 by registration')
         vv.xlabel('x'), vv.ylabel('y'), vv.zlabel('z')
         a4b.daspect = 1, 1, -1
         
